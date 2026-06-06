47c20da050cf21a88ca8ab6b156188cd2a966336
arsal3646 <arsal3646@gmail.com>
2026-06-06 23:34:42 +0400

    Setup done

--- 01_R_Basics.R
+++ 01_R_Basics.R
@@ -4,3 +4,13 @@ x <- 10
 y <- 20
 
 x + y
+
+# R Practice
+
+This repository contains my R programming practice files.
+
+Learning journey:
+- R Basics
+- Data Structures
+- Statistics
+- Data Analysis
